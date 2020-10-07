ecb9b13568fa152324e67e86e3b63e72e97d184e
rockplayer007 <maildiroland@yahoo.it>
2020-10-07 18:48:08 +0200

    removed comments

--- src/content.js
+++ src/content.js
@@ -1,8 +1,3 @@
-//alert('ciao')
-//setTimeout(() => {  console.log(sessionTimer.interval); }, 2000);
-//console.log('ciaooooooooooo')
-//console.log(clearInterval(sessionTimer.interval))
-
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('background.js');
 (document.head||document.documentElement).appendChild(s);
